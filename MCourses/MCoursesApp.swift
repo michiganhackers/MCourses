32b5ec5788844f119eb8b59ac654304886aedc34
Finn Moore <fdm@umich.edu>
2023-10-26 18:06:06 -0400

    Initial Commit

---
+++ MCourses/MCoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCoursesApp.swift
+//  MCourses
+//
+//  Created by Finn Moore on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct MCoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
