6ca4b873b0066ea988cb44ea431bcf996addd9b1
Finn Moore <fdm@umich.edu>
2023-10-26 18:06:57 -0400

    Change globe to hat

--- MCourses/ContentView.swift
+++ MCourses/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "graduationcap")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("MCourses")
         }
         .padding()
     }
