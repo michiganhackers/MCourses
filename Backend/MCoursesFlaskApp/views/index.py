971b0fb6dca7effe7160f9baebfdce9e3404a1f9
Parker Mathena <pmathena@umich.edu>
2023-11-16 21:18:59 -0500

    Terms and schools endpoints

--- Backend/MCoursesFlaskApp/views/index.py
+++ Backend/MCoursesFlaskApp/views/index.py
@@ -6,8 +6,44 @@ URLs include:
 """
 import flask
 import MCoursesFlaskApp
+import requests
+import json
+
+ACCESS_TOKEN = ""
+TERMS = {}
+TERM_TO_SCHOOLS_DICT = {}
 
 # flask --app MCoursesFlaskApp --debug run --host 0.0.0.0 --port 8000
+
+
+# TODO Add in expired (?) access token guard, may need to add in a get_access_token func then also
+def UM_API_get_terms():
+    global TERMS
+    if TERMS != {}:
+        return TERMS
+
+    api_url = "https://gw.api.it.umich.edu/um/Curriculum/SOC/Terms"
+    headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
+    response = requests.get(api_url, headers=headers)
+    response_dict = response.json()
+    TERMS = response_dict["getSOCTermsResponse"]["Term"]
+    return TERMS
+
+def UM_API_get_schools_for_term(term_code):
+    print("TYPE OF term_code:")
+    print(type(term_code))
+    assert(type(term_code) == int)
+    global TERM_TO_SCHOOLS_DICT
+    if TERM_TO_SCHOOLS_DICT.__contains__(term_code):
+        return TERM_TO_SCHOOLS_DICT[term_code]
+    
+    api_url = f"https://gw.api.it.umich.edu/um/Curriculum/SOC/Terms/{term_code}/Schools"
+    headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
+    response = requests.get(api_url, headers=headers)
+    response_dict = response.json()
+    # TERM_TO_SCHOOLS_DICT[term_code] = 
+
+    return response.json()
 
 
 @MCoursesFlaskApp.app.route('/api/', methods=["GET"])
@@ -35,6 +71,26 @@ def get_courses():
     }
 
     return flask.jsonify(**context)
+
+
+@MCoursesFlaskApp.app.route('/api/terms/', methods=["GET"])
+def get_terms():
+    """Return a list of all terms."""
+    response_dict = [] # older value? Maybe if (cond): update
+    # I figured we may want to call this conditionally and think it's a nice abstraction anyway
+    if True:
+        response_dict = UM_API_get_terms()
+
+    return flask.jsonify(response_dict)
+
+
+@MCoursesFlaskApp.app.route('/api/schools/', methods=["GET"])
+def get_schools():
+    # * atm, written for most recent term
+    terms = UM_API_get_terms()
+    schools = UM_API_get_schools_for_term(terms[0]["TermCode"])
+
+    return flask.jsonify(schools)
 
 
 @MCoursesFlaskApp.app.route('/api/courses/<course_id>', methods=["GET"])
