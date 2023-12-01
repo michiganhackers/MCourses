882c063b5299f68cdca96be226dbf439d485f011
chenww1 <113079613+chenww1@users.noreply.github.com>
2023-11-30 19:31:51 -0500

    Add files via upload

--- MCourses/ContentView.swift
+++ MCourses/ContentView.swift
@@ -6,19 +6,65 @@
 //
 
 import SwiftUI
+import UIKit
+import WebKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "graduationcap")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("MCourses")
+        NavigationView {
+            VStack {
+                Image(systemName: "graduationcap")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .padding()
+                
+                Text("MCourses")
+                    .font(.largeTitle)
+                    .padding()
+                
+                NavigationLink(
+                    destination: WebView(url: URL(string: "https://weblogin.umich.edu")!),
+                    label: {
+                        Text("Login")
+                            .font(.title)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                )
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct WebView: View {
+    let url: URL
+
+    var body: some View {
+        SwiftUIWebView(url: url)
+            .navigationBarTitle("", displayMode: .inline)
+    }
+}
+
+struct SwiftUIWebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> UIView {
+        let webView = WKWebView()
+        return webView
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        if let uiWebView = uiView as? WKWebView {
+            let request = URLRequest(url: url)
+            uiWebView.load(request)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
