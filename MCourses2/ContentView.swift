c2dcf6252dc46e5524a6f1114b3685c6ecc647f8
Ishani Das <ishanidas05@gmail.com>
2024-03-28 19:24:53 -0400

    Initial Commit

---
+++ MCourses2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MCourses2
+//
+//  Created by Ishani Das on 3/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MCourses2/MCourses2App.swift
@@ -0,0 +1,17 @@
+//
+//  MCourses2App.swift
+//  MCourses2
+//
+//  Created by Ishani Das on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MCourses2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
