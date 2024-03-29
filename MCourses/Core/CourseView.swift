8105e36448361f62a4fad9dba9cdad80e7dc8dc2
Tzeyi <tytiong2@gmail.com>
2024-03-28 20:47:04 -0400

    Adding reviews to CourseView

--- MCourses/Core/CourseView.swift
+++ MCourses/Core/CourseView.swift
@@ -18,6 +18,13 @@ struct CourseView: View {
         self.courseViewModel = CourseViewModel(course: course)
     }
     
+    var body: some View {
+        VStack() {
+            header
+            reviewPage
+        }
+    }
+    
     var header: some View {
         VStack {
             ZStack {
@@ -36,7 +43,7 @@ struct CourseView: View {
                     .foregroundColor(.black)
                     .padding()
                     .background(maize, in: RoundedRectangle(cornerRadius: 10))
-
+                
                 Text("\(courseViewModel.course.credits) Credits")
                     .fontWeight(.medium)
                     .foregroundColor(.black)
@@ -47,22 +54,78 @@ struct CourseView: View {
         }
     }
     
-    var body: some View {
-        VStack() {
-            header
+    var reviewPage: some View {
+        VStack(alignment: .leading) {
+            Text("Reviews")
+                .font(.largeTitle)
+                .bold()
+            ScrollView (){
+                ForEach(courseViewModel.course.reviews) { review in
+                    VStack (spacing: 30){
+                        HStack (){
+                            Text("\(review.semester)")
+                            Spacer()
+                            Text("\(review.professor)")
+                            Spacer()
+                            Text("\(review.rating)")
+                        }
+                        .bold()
+                        
+                        Text("\(review.review)")
+                        
+                    }
+                }
+            }
+            .background(Color.gray.brightness(0.40))
         }
+//        .padding(0)
+//        .frame(maxWidth: .infinity)
     }
 }
 
+// -------- Temporary Variables for Testing ---------
+
+let sampleReview1 = Review(
+    id: "1",
+    course: "Data Structures and Algorithms",
+    semester: "Winter 2023",
+    professor: "Jonathan Beaumont",
+    review: "Enrolling in this course was a transformative experience that exceeded my expectations. The curriculum was well-structured, offering a comprehensive overview of the subject matter while delving deep into key concepts. The instructors demonstrated exceptional expertise, providing clear explanations and real-world applications that enhanced my understanding.",
+    rating: 9.8,
+    workload: 58,
+    worth: 40,
+    enjoyment: 61,
+    upvote: 6
+)
+
+let sampleReview2 = Review(
+    id: "2",
+    course: "Data Structures and Algorithms",
+    semester: "Fall 2023",
+    professor: "Emily Graetz",
+    review: "Hello World",
+    rating: 8,
+    workload: 50,
+    worth: 20,
+    enjoyment: 100,
+    upvote: 10
+)
+
+let sampleCourse = Course(
+    name: "Data Structures and Algorithms",
+    department: "EECS",
+    number: 281,
+    credits: 4,
+    description: "Introduction to the algorithm analysis and O-notation; Fundamental data structures including lists, stacks, queues, priority queues, hash tables, binary trees, search trees, balanced, trees, and graphs; searching and sorting algorithms; recursive algorithms; basic graph algorithms; introduction to greedy algorithms and divide and conquer strategy. Several programming assignments.",
+    avgRating: 4.5,
+    avgWorkload: 6.8,
+    avgWorth: 7.7,
+   avgEnjoyment: 3.4,
+   reviews: [sampleReview1, sampleReview2])
+
+
+// --------------------- end -------------------------
+
 #Preview {
-    CourseView(course: Course(name: "Data Structures and Algorithms",
-   department: "EECS",
-   number: 281,
-   credits: 4,
-   description: "Introduction to the algorithm analysis and O-notation; Fundamental data structures including lists, stacks, queues, priority queues, hash tables, binary trees, search trees, balanced, trees, and graphs; searching and sorting algorithms; recursive algorithms; basic graph algorithms; introduction to greedy algorithms and divide and conquer strategy. Several programming assignments.",
-   avgRating: 4.5,
-   avgWorkload: 6.8,
-   avgWorth: 7.7,
-  avgEnjoyment: 3.4,
-  reviews: []))
+    CourseView(course: sampleCourse)
 }
