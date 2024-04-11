767c4947501b84f9ed9550b86210b83af84d31c6
Finn Moore <fdm@umich.edu>
2024-04-11 19:50:54 -0400

    Courseview thingy

--- MCourses/Core/CourseView.swift
+++ MCourses/Core/CourseView.swift
@@ -18,39 +18,143 @@ struct CourseView: View {
         self.courseViewModel = CourseViewModel(course: course)
     }
     
+
+    
     var header: some View {
-        VStack {
-            ZStack {
-                Rectangle()
-                    .frame(height: 200.0)
-                    .foregroundColor(blue)
+        ZStack {
+            Rectangle()
+                .frame(height: 200.0)
+                .foregroundColor(blue)
+            VStack{
                 Text(courseViewModel.course.name)
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                     .fontWeight(.bold)
                     .foregroundColor(Color.white)
+                HStack(spacing: 20, content: {
+                    Text("\(courseViewModel.course.department) \(courseViewModel.course.number)")
+                        .fontWeight(.medium)
+                        .foregroundColor(.black)
+                        .padding()
+                        .background(maize, in: RoundedRectangle(cornerRadius: 10))
+                    
+                    Text("\(courseViewModel.course.credits) Credits")
+                        .fontWeight(.medium)
+                        .foregroundColor(.black)
+                        .padding()
+                        .background(maize, in: RoundedRectangle(cornerRadius: 10))
+                })
+            }.offset(y:50)
+        }
+        
+    }
+    
+    var score: some View {
+        HStack(alignment: .center, spacing: 10) {
+            Text("\(courseViewModel.course.avgRating, specifier: "%.1f")/10")
+              .font(.title)
+              .bold()
+              .multilineTextAlignment(.center)
+              .foregroundColor(Color(red: 1, green: 0.8, blue: 0.02))
+        }
+        .padding(.horizontal, 32)
+        .padding(.vertical, 16)
+        .background(Color(red: 0, green: 0.15, blue: 0.3))
+        .cornerRadius(16)
+    }
+    
+    var statistics: some View {
+        VStack(alignment: .leading) {
+            Text("Statistics").font(.title).bold()
+            Text("Workload").font(.headline).bold()
+            ZStack(alignment: .leading) {
+                Rectangle()
+                  .foregroundColor(.clear)
+                  .frame(width: 337, height: 28)
+                  .background(Color(red: 0.85, green: 0.85, blue: 0.85))
+                  .cornerRadius(32)
+                Rectangle()
+                  .foregroundColor(.clear)
+                  .frame(width: courseViewModel.course.avgWorkload*337/10, height: 28)
+                  .background(Color(red: 0, green: 0.15, blue: 0.3))
+                  .cornerRadius(22)
+                Text("\(Int(courseViewModel.course.avgWorkload*10))%")
+                    .font(.caption).bold()
+                  .foregroundColor(.white)
+                  .offset(x:10)
             }
-            .ignoresSafeArea()
-            HStack(spacing: 20, content: {
-                Text("\(courseViewModel.course.department) \(courseViewModel.course.number)")
-                    .fontWeight(.medium)
-                    .foregroundColor(.black)
-                    .padding()
-                    .background(maize, in: RoundedRectangle(cornerRadius: 10))
+            Text("Was it worth it?").font(.headline).bold()
+            ZStack(alignment: .leading) {
+                Rectangle()
+                  .foregroundColor(.clear)
+                  .frame(width: 337, height: 28)
+                  .background(Color(red: 0.85, green: 0.85, blue: 0.85))
+                  .cornerRadius(32)
+                Rectangle()
+                  .foregroundColor(.clear)
+                  .frame(width: courseViewModel.course.avgWorth*337/10, height: 28)
+                  .background(Color(red: 0, green: 0.15, blue: 0.3))
+                  .cornerRadius(22)
+                Text("\(Int(courseViewModel.course.avgWorth*10))%")
+                    .font(.caption).bold()
+                  .foregroundColor(.white)
+                  .offset(x:10)
+            }
+            Text("Enjoyment").font(.headline).bold()
+            ZStack(alignment: .leading) {
+                Rectangle()
+                  .foregroundColor(.clear)
+                  .frame(width: 337, height: 28)
+                  .background(Color(red: 0.85, green: 0.85, blue: 0.85))
+                  .cornerRadius(32)
+                Rectangle()
+                  .foregroundColor(.clear)
+                  .frame(width: courseViewModel.course.avgEnjoyment*337/10, height: 28)
+                  .background(Color(red: 0, green: 0.15, blue: 0.3))
+                  .cornerRadius(22)
+                Text("\(Int(courseViewModel.course.avgEnjoyment*10))%")
+                    .font(.caption).bold()
+                  .foregroundColor(.white)
+                  .offset(x:10)
+            }
+        }
+    }
+    // For expandable on tap description
+    @State var isViewed = false
+    var description: some View {
+        Button {
+                isViewed.toggle()
+        } label: {
+            Text("\(courseViewModel.course.description)")
+                .font(.body).foregroundColor(.black)
+                .multilineTextAlignment(.leading)
+                .frame(width: 337, height: isViewed ? nil : 100)
+        }.padding()
+    }
+    
+    var writeReviewButton: some View {
+        Button {
+            // write review
+        } label: {
+            Text("Write Review")
+                .fontWeight(.medium)
+                .foregroundColor(.black)
+                .padding()
+                .background(maize, in: RoundedRectangle(cornerRadius: 10))
+                .padding()
 
-                Text("\(courseViewModel.course.credits) Credits")
-                    .fontWeight(.medium)
-                    .foregroundColor(.black)
-                    .padding()
-                    .background(maize, in: RoundedRectangle(cornerRadius: 10))
-            })
-            .offset(y:-50)
+                
         }
     }
     
     var body: some View {
-        VStack() {
+        ScrollView {
             header
+            description
+            score
+            statistics
+            writeReviewButton
         }
+        .ignoresSafeArea()
     }
 }
 
